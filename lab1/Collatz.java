f227f76a9dd0c9230a3e3e4bf2c14b962ec20b36
hygroupseries <gsws_2020@qq.com>
2025-01-23 20:42:22 +0800

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,12 +5,25 @@ public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
+//        if (n  == 128) {
+//            return 1;
+//        } else if (n == 5) {
+//            return 3 * n + 1;
+//        } else {
+//            return n * 2;
+//        }
+//    }
+        while(true){
+            if(n == 1){
+                return 1;
+            }
+            else
+            {if (n % 2 == 0){
+                return n = n / 2;
+            }
+            else{
+                return n = 3*n + 1;
+            }}
         }
     }
 
